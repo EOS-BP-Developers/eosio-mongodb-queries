cac6640588bd0701e8d3bcdfee8c368d8a0421ac
Denis Carriere <carriere.denis@gmail.com>
2018-07-31 12:19:05 -0400

    Remove stringify

--- test/getActions.test.ts
+++ test/getActions.test.ts
@@ -9,6 +9,6 @@ import { connect } from "../config";
         match: {"data.from": "eosnationftw", "data.receiver": "eosnationftw"},
         sort: {_id: -1},
     });
-    console.log(JSON.stringify(await test1.toArray(), null, 4));
+    console.log(await test1.toArray());
     client.close();
 })();
